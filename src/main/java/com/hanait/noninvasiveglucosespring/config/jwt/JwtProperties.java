9aa204f8ca23ac19cb23c1d445ec6f28e1e6dada
dcbaek <ehdco8988@gmail.com>
2022-11-23 16:56:02 +0900

    remove cached

--- src/main/java/com/hanait/noninvasiveglucosespring/config/jwt/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.hanait.noninvasiveglucosespring.config.jwt;
-
-public interface JwtProperties {
-    String SECRET = "hanait1010";
-    int EXPIRATION_TIME = 1000000;
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
-}
